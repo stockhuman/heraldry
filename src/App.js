262216ccc4acf21fec4f384e94747ec73a662f26
Michael Hemingway <mpghemingway@gmail.com>
2019-05-08 11:05:25 -0400

    CSS styling and organisation

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import Generator from './Generator'
-import './assets/css/App.css';
+import './assets/GutenbergTextura.ttf'
+import './assets/css/App.css'
 
 function App() {
 	return (
 		<div className="App">
-			<header>Heraldry Generator</header>
+			<header>Heraldry.io</header>
 			<main>
 				<Generator />
 			</main>
+			<footer>Made with honour and chivalry by <a href="https://michaelhemingway.com">Michael Hemingway</a></footer>
 		</div>
 	);
 }
