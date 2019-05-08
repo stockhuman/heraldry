2baf20f5b285d34e961c5c4f957339bf6a0a78a5
Michael Hemingway <mpghemingway@gmail.com>
2019-05-08 18:15:18 -0400

    Ally compat

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 		<div className="App">
 			<header>Heraldry.io</header>
 			<main>
-				<span style={{display: 'block', textAlign: 'center', fontSize: 150}}>👑</span>
+				<span role="img" style={{display: 'block', textAlign: 'center', fontSize: 150}} aria-label="crown">👑</span>
 				<Generator />
 			</main>
 			<footer>Made with honour and chivalry by <a href="https://michaelhemingway.com">Michael Hemingway</a></footer>
