4acf21bddaf0b13bc8216f3923b55edc609e9480
Michael Hemingway <mpghemingway@gmail.com>
2019-06-13 23:45:57 -0400

    pushing build

---
+++ build/precache-manifest.d5052af57fdc3089c03cc6a666a6f1b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43def3c87191b97959e6095ef3857192",
+    "url": "/heraldry/build/index.html"
+  },
+  {
+    "revision": "8565a230d479227d9d4d",
+    "url": "/heraldry/build/static/css/main.b9ec4647.chunk.css"
+  },
+  {
+    "revision": "52d770626603dd161de2",
+    "url": "/heraldry/build/static/js/2.e9c06cc0.chunk.js"
+  },
+  {
+    "revision": "8565a230d479227d9d4d",
+    "url": "/heraldry/build/static/js/main.4c47d7ac.chunk.js"
+  },
+  {
+    "revision": "faf0b2a35a90c80837ac",
+    "url": "/heraldry/build/static/js/runtime~main.d632988f.js"
+  },
+  {
+    "revision": "8c0b792525b6692bfe6dcef8c583ef16",
+    "url": "/heraldry/build/static/media/GutenbergTextura.8c0b7925.ttf"
+  },
+  {
+    "revision": "adbb78af529d9149b6f6f9e756909fcf",
+    "url": "/heraldry/build/static/media/seguiemj.adbb78af.ttf"
+  }
+]);
